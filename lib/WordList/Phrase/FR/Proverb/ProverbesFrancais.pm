f3d953ed892a1292aa7e41d491b3cb3be2362e64
perlancar (@pc-office) <perlancar@gmail.com>
2016-02-04 23:09:13 +0700

    Initial commit

---
+++ lib/WordList/Phrase/FR/Proverb/ProverbesFrancais.pm
@@ -0,0 +1,20 @@
+package WordList::Phrase::FR::Proverb::ProverbesFrancais;
+
+# DATE
+# VERSION
+
+use utf8;
+
+use WordList;
+our @ISA = qw(WordList);
+
+# STATS
+
+1;
+# ABSTRACT: French proverbs from proverbesfrancais.com
+
+=head1 SEE ALSO
+
+=cut
+
+__DATA__
